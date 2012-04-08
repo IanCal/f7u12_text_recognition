699bb0ed7d2f2201506c675d7a905823732dee54
Ian Calvert <ianjcalvert@gmail.com>
2012-04-08 11:26:47 +0100

    Added recognition code, with simple comparison function to known letters, source images are broken due to case insensitivity of filenames

--- letterSegmentation.py
+++ letterSegmentation.py
@@ -8,7 +8,7 @@ def findContours(img):
     return FindContours(temp, storage, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE)
 
 def getGreyScale(img):
-    grey = CreateImage(GetSize(src),8,1) 
+    grey = CreateImage(GetSize(img),8,1) 
     CvtColor(img,grey,CV_BGR2GRAY)
     return grey
 
@@ -29,17 +29,40 @@ def mapSequence(seq, onItem):
 def getROI(contour):
     return BoundingRect(list(contour))
 
+
+def similarity(im1, im2):
+    # im1 is the bigger of the two, if not, swap them
+    if GetSize(im1)[0] < GetSize(im2)[0]:
+        im1, im2 = im2, im1
+    size = GetSize(im1)
+    scaledIm2 = CreateImage(size, 8, 1) 
+    Resize(im2, scaledIm2)
+    return 1 - ((1.0 / (255. * size[0] * size[1])) * Norm(im1, scaledIm2))
+    
+    
+
 def recogniseLetterConstructor(sourceImage, letterExamples):
     def recogniseLetter(roi):
         SetImageROI(sourceImage, roi)
-        probabilities = map(lambda (letter, image): (letter, similarity(sourceImage, image)), letterExamples)
-        probabilities.sort(reverse=True, key=itemgetter(2))
+        probabilities = map(lambda (letter, image): (roi, letter, similarity(sourceImage, image)), letterExamples)
+        ResetImageROI(sourceImage)
+        probabilities.sort(reverse=True, key=lambda (roi, l, p): p)
         return probabilities[0]
     return recogniseLetter
 
+def safeLoad(imageLocation):
+    try:
+        im = LoadImage(imageLocation)
+        return getGreyScale(im)
+    except:
+        return None
+
 def loadLetterExamples():
     alphabet = map(chr, range(ord('a'), ord('z')+1) + range(ord('A'), ord('Z')+1))
-    return map(lambda letter: (letter, LoadImage("letters/%s.png"%letter)), alphabet)
+    images = map(lambda letter: (letter, safeLoad("letters/%s.png"%letter)), alphabet)
+    return filter(lambda (letter, image): image != None, images)
+
+
 
 src = LoadImage("test.png") 
 thresholded = getThresholdedImage(src)
@@ -48,5 +71,15 @@ rois = mapSequence(contours, getROI)
 letterExamples = loadLetterExamples()
 letterCandidates = filter(lambda (x,y,w,h): (h > 3) and (w < 20 and h < 20), rois)
 letterSuggestions = map(recogniseLetterConstructor(thresholded, letterExamples), letterCandidates)
-letters = filter(lambda (roi, letter, p): p > 0.5, letterSuggestions)
+
+for i, (roi, letter, p) in enumerate(letterSuggestions):
+    SetImageROI(src, roi)
+    SaveImage("out/%s-%f.png"%(letter, p), src)
+
+
+#letters = filter(lambda (roi, letter, p): p > 0.95, letterSuggestions)
+
+#letters.sort(key=lambda ((x,y,w,h),l,p): (y+h))
+
+#print map(lambda (roi, l, p) : l, letters)
 
